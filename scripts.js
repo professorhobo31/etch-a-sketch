2c6a57a9831709a9d5802129d28d52ce12bef60e
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-02-20 17:54:40 -0300

    Implemented eventlisteners and test function

--- scripts.js
+++ scripts.js
@@ -3,9 +3,14 @@ let container = document.getElementsByClassName('container');
 for (let i = 0; i < 256; i++) {
     let gridElement = document.createElement('div');
     gridElement.classList.add('gridElement');
-    gridElement.textContent = i;
-    container[0].appendChild(gridElement)
+    // gridElement.textContent = i;
+    container[0].appendChild(gridElement);
 }
 
 let gridElements = document.getElementsByClassName('gridElement');
 console.log(gridElements);
+
+for (let i = 0; i < gridElements.length; i++) {
+    gridElements[i].textContent = i;
+    gridElements[i].addEventListener('click', () => alert("Clicking works"));    
+}
