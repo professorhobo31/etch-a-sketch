5a5334f39f668d2f30ec557b809a0425662d7b0a
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-02-20 18:32:22 -0300

    Add initial black pixel function

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 let container = document.getElementsByClassName('container');
  
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 256; i++) {                                     //function creates the divs for the board and appends to container div
     let gridElement = document.createElement('div');
     gridElement.classList.add('gridElement');
     // gridElement.textContent = i;
     container[0].appendChild(gridElement);
 }
 
-let gridElements = document.getElementsByClassName('gridElement');
-console.log(gridElements);
+let gridElements = document.getElementsByClassName('gridElement');  //creates an object out of the created divs
+// console.log(gridElements);
 
-for (let i = 0; i < gridElements.length; i++) {
-    gridElements[i].textContent = i;
-    gridElements[i].addEventListener('click', () => alert("Clicking works"));    
+function pixelColoring(evt) {
+    evt.target.style.cssText = 'background-color: black'
+}
+
+for (let i = 0; i < gridElements.length; i++) {                     //uses the created object to asign an eventlistener to every square
+    // gridElements[i].textContent = i;
+    gridElements[i].addEventListener('click', pixelColoring);    
 }
