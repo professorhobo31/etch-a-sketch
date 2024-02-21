d86f42ce6e49c3e1557a99f2c27d9128c8285083
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-02-21 11:27:49 -0300

    Add basic paint-erase behavior and left panel

--- scripts.js
+++ scripts.js
@@ -10,11 +10,20 @@ for (let i = 0; i < 256; i++) {                                     //function c
 let gridElements = document.getElementsByClassName('gridElement');  //creates an object out of the created divs
 // console.log(gridElements);
 
-function pixelColoring(evt) {
-    evt.target.style.cssText = 'background-color: black'
+function pixelColoring(evt) {                                       //uses the event target attribute to know which square to paint
+    let element = evt.target;
+    let styles = getComputedStyle(element);
+    let currentBackground = styles.getPropertyValue('background-color');        //checks background color to toggle between paint and erase
+    if (currentBackground == 'rgb(255, 255, 255)' || currentBackground == 'rgb(169, 169, 169)') { 
+        evt.target.style.cssText = 'background-color: black';       //"paint"
+    }
+    else if (currentBackground == 'rgb(0, 0, 0)') {
+        evt.target.style.cssText = 'background-color: white';       //"erase"
+    }
+    console.log(currentBackground);
 }
 
 for (let i = 0; i < gridElements.length; i++) {                     //uses the created object to asign an eventlistener to every square
-    // gridElements[i].textContent = i;
     gridElements[i].addEventListener('click', pixelColoring);    
 }
+
