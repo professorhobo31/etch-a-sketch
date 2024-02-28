582b40bcc9ed67ab6f5cee3aea2e25e6c58e76bb
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-02-28 19:13:09 -0300

    Add click&hold drawing to the grid
    Created a new variable that stores the state of the mouseclick,
    added a check for that variable in the pixelColoring. Can now click
    and hold the mouse to paint and erase whatever you hover at that time.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 let container = document.getElementsByClassName('container');
 let resolution = 16;
-let divSize = (960/resolution) - 2;
- 
-for (let i = 0; i < (resolution * resolution); i++) {               //function creates the divs for the board and appends to container div
+let divSize = (960 / resolution) - 2;
+
+let mouseDown = false                                               //main idea is to store the state of the mouse on a var and check it later
+document.onmousedown = () => (mouseDown = true)
+document.onmouseup = () => (mouseDown = false)
+
+for (let i = 0; i < (resolution * resolution); i++) {               //function creates the initial divs for the board and appends to container div
     let gridElement = document.createElement('div');
     gridElement.classList.add('gridElement');
     gridElement.style.height = divSize + 'px';
@@ -11,59 +15,68 @@ for (let i = 0; i < (resolution * resolution); i++) {               //function c
     container[0].appendChild(gridElement);
 }
 
-let gridElements = document.getElementsByClassName('gridElement');  //creates an object out of the created divs
-// console.log(gridElements);
+let gridElements = document.getElementsByClassName('gridElement');  //variable contains the divs created in the previous step
 
-function pixelColoring(evt) {                                       //uses the event target attribute to know which square to paint
+/**
+ * Function checks background color of div and switches to its opposite, black or white. It uses the event target attribute to know which square to paint.
+ * 
+ * @param {Event} evt 
+ */
+function pixelColoring(evt) {
+    if (evt.type === 'mouseover' && !mouseDown) { return }
     let element = evt.target;
     let styles = getComputedStyle(element);
-    let currentBackground = styles.getPropertyValue('background-color');        //checks background color to toggle between paint and erase
-    if (currentBackground == 'rgb(255, 255, 255)' || currentBackground == 'rgb(169, 169, 169)') { 
-        evt.target.style.backgroundColor =  'black';                            //"paint"
+    let currentBackground = styles.getPropertyValue('background-color');
+    if (currentBackground == 'rgb(255, 255, 255)' || currentBackground == 'rgb(169, 169, 169)') {
+        evt.target.style.backgroundColor = 'black';
     }
-    else if (currentBackground == 'rgb(0, 0, 0)') {
-        evt.target.style.backgroundColor = 'white';                             //"erase" 
+    if (currentBackground == 'rgb(0, 0, 0)') {
+        evt.target.style.backgroundColor = '';
     }
-    console.log(currentBackground);
+    console.log(mouseDown);
 }
 
 for (let i = 0; i < gridElements.length; i++) {                     //uses the created object to asign an eventlistener to every square
-    gridElements[i].addEventListener('click', pixelColoring);    
+    gridElements[i].addEventListener('mousedown', pixelColoring);
+    gridElements[i].addEventListener('mouseover', pixelColoring);
 }
 
-
 let display = document.getElementById('resDisplay');
-let slider = document.getElementById('slider').oninput = function() {
+let slider = document.getElementById('slider').oninput = function () {
     let value = this.value;
-    display.textContent = value + "x" + value;         //need a way to set the resolution global variable from here
+    display.textContent = value + "x" + value;
     resolution = this.value
     console.log(resolution)
 }
 
 const resButton = document.getElementById('resButton');
 
+/**
+ * First deletes the grid, then creates a new one using the new resolution set by the slider and adds eventlisteners for them to work as intended.
+ */
 function setResolution() {
-    const grid = document.querySelectorAll('.gridElement');  //select already drawn grids
+    const grid = document.querySelectorAll('.gridElement');
 
-    grid.forEach(box => {          //delete em
+    grid.forEach(box => {
         box.remove();
     })
 
-    divSize = (960/resolution) - 2;     //calculate new div size matching new resolution
+    divSize = (960 / resolution) - 2;     //calculate new div size matching new resolution
 
     for (let i = 0; i < (resolution * resolution); i++) {       //redraw
-         let gridElement = document.createElement('div');
-         gridElement.classList.add('gridElement');
-         gridElement.style.height = divSize + 'px';
-         gridElement.style.width = divSize + 'px';
+        let gridElement = document.createElement('div');
+        gridElement.classList.add('gridElement');
+        gridElement.style.height = divSize + 'px';
+        gridElement.style.width = divSize + 'px';
         //  gridElement.textContent = divSize;
-         container[0].appendChild(gridElement);
+        container[0].appendChild(gridElement);
     }
 
-    let gridElements = document.getElementsByClassName('gridElement');  //creates an object out of the created divs
+    gridElements = document.getElementsByClassName('gridElement');  //stores variable out of the created divs
 
-    for (let i = 0; i < gridElements.length; i++) {                     //uses the created object to asign an eventlistener to every square
-        gridElements[i].addEventListener('click', pixelColoring);    
+    for (let i = 0; i < gridElements.length; i++) {                     //uses the variable to asign an eventlistener to every square
+        gridElements[i].addEventListener('mousedown', pixelColoring);
+        gridElements[i].addEventListener('mouseover', pixelColoring);
     }
 }
 resButton.addEventListener('click', setResolution);
