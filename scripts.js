81e4c8103c965831841864f6fc31d8b6c559b238
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-03-04 16:54:33 -0300

    Add buttons and greyscale functionality
    Introduced new variables that toggle between states and modified the
    pixelColoring function to check this variables in order to accomodate
    the new greyscale mode which paints each div in a greyscale pallette
    that increases in darkness by 10%.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let container = document.getElementsByClassName('container');
 let resolution = 16;
 let divSize = (960 / resolution) - 2;
+let currentMode = 'blackWhite';
+let currentBrush = 'paint';
 
-let mouseDown = false                                               //main idea is to store the state of the mouse on a var and check it later
-document.onmousedown = () => (mouseDown = true)
-document.onmouseup = () => (mouseDown = false)
+let mouseDown = false;                                 //main idea is to store the state of the mouse on a var and check it later
+document.onmousedown = () => (mouseDown = true);
+document.onmouseup = () => (mouseDown = false);
 
 for (let i = 0; i < (resolution * resolution); i++) {               //function creates the initial divs for the board and appends to container div
     let gridElement = document.createElement('div');
@@ -18,7 +20,7 @@ for (let i = 0; i < (resolution * resolution); i++) {               //function c
 let gridElements = document.getElementsByClassName('gridElement');  //variable contains the divs created in the previous step
 
 /**
- * Function checks background color of div and switches to its opposite, black or white. It uses the event target attribute to know which square to paint.
+ * Function checks background color of div and paints it full black or on a greyscale by 10% increments. It uses the event target attribute to know which square to paint.
  * 
  * @param {Event} evt 
  */
@@ -27,10 +29,48 @@ function pixelColoring(evt) {
     let element = evt.target;
     let styles = getComputedStyle(element);
     let currentBackground = styles.getPropertyValue('background-color');
-    if (currentBackground == 'rgb(255, 255, 255)' || currentBackground == 'rgb(169, 169, 169)') {
+
+    if (currentBrush == 'paint' && currentMode == 'blackWhite') {
         evt.target.style.backgroundColor = 'black';
     }
-    if (currentBackground == 'rgb(0, 0, 0)') {
+
+    else if (currentBrush == 'paint' && currentMode == 'greyscale') {
+        if (currentBackground == 'rgb(255, 255, 255)' || currentBackground == 'rgb(169, 169, 169)') {
+            evt.target.style.backgroundColor = 'rgb(229, 229, 229)';
+        }
+        else if (currentBackground == 'rgb(229, 229, 229)') {
+            evt.target.style.backgroundColor = 'rgb(204, 204, 204)';
+        }
+        else if (currentBackground == 'rgb(204, 204, 204)') {
+            evt.target.style.backgroundColor = 'rgb(179, 179, 179)';
+        }
+        else if (currentBackground == 'rgb(179, 179, 179)') {
+            evt.target.style.backgroundColor = 'rgb(153, 153, 153)';
+        }
+        else if (currentBackground == 'rgb(153, 153, 153)') {
+            evt.target.style.backgroundColor = 'rgb(128, 128, 128)';
+        }
+        else if (currentBackground == 'rgb(128, 128, 128)') {
+            evt.target.style.backgroundColor = 'rgb(102, 102, 102)';
+        }
+        else if (currentBackground == 'rgb(102, 102, 102)') {
+            evt.target.style.backgroundColor = 'rgb(77, 77, 77)';
+        }
+        else if (currentBackground == 'rgb(77, 77, 77)') {
+            evt.target.style.backgroundColor = 'rgb(51, 51, 51)';
+        }
+        else if (currentBackground == 'rgb(51, 51, 51)') {
+            evt.target.style.backgroundColor = 'rgb(26, 26, 26)';
+        }
+        else if (currentBackground == 'rgb(26, 26, 26)') {
+            evt.target.style.backgroundColor = 'rgb(0, 0, 0)';
+        }
+        else {
+            return
+        }
+    }
+
+    else if (currentBrush == 'erase') {
         evt.target.style.backgroundColor = '';
     }
     console.log(mouseDown);
@@ -80,3 +120,26 @@ function setResolution() {
     }
 }
 resButton.addEventListener('click', setResolution);
+
+const eraseButton = document.getElementById('eraser');
+eraseButton.addEventListener('click', () => {
+    currentBrush = 'erase';
+});
+
+const paintButton = document.getElementById('painter');
+paintButton.addEventListener('click', () => {
+    currentBrush = 'paint';
+});
+
+const modeButton = document.getElementById('mode');
+modeButton.addEventListener('click', () => {
+    if (currentMode == 'blackWhite') {
+        currentMode = 'greyscale'
+        modeButton.textContent = 'Greyscale Mode'
+    }
+    else {
+        currentMode = 'blackWhite';
+        modeButton.textContent = 'B/W Mode'
+    }
+})
+
